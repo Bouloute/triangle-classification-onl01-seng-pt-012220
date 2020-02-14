00c61e29f59423aedc612b10aa237e5c7e287dc9
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-14 23:57:52 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
+require ('pry')
 class Triangle
-  # write code here
+
+  def initialize(side_a, side_b, side_c)
+
+    @sides = [side_a, side_b, side_c]
+
+    if    ((side_a == side_b) && (side_a == side_c))
+      @kind = :equilateral
+    elsif ((side_a == side_b) || (side_a == side_c) || (side_b == side_c))
+      @kind = :isosceles
+    elsif ((side_a != side_b) && (side_a != side_c) && (side_b != side_c))
+      @kind = :scalene
+    end
+  end
+
+  def kind
+    binding.pry
+    if @kind == nil || @sides.include?{|side| side <= 0}
+      begin
+        raise TriangleError
+      rescue TriangleError => error
+          puts error.message
+      end
+    else
+      return @kind
+    end
+  end
+
+  class TriangleError  < StandardError
+    def message
+      "TriangleError"
+    end
+  end
 end
+
+
+puts Triangle.new(0, 0, 0).kind
