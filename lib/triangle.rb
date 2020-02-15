760d01a52d2810ccf6fbf5341cf0f1c048b02814
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-15 00:46:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -33,7 +33,7 @@ class Triangle
     if @sides[0] + @sides[1] < @sides[2] || @sides[0] + @sides[2] < @sides[1] || @sides[1] + @sides[2] < @sides[0]
       return false
     end
-    
+
 binding.pry
     true
   end
