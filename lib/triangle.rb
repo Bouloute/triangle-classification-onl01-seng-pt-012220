8e0921dd03e703f9f21ca08bfc9994efd6d524b8
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-15 00:13:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
 
   def kind
-    binding.pry
+    #binding.pry
     if    ((side_a == side_b) && (side_a == side_c))
       kind = :equilateral
     elsif ((side_a == side_b) || (side_a == side_c) || (side_b == side_c))
