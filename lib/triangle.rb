d42d83a94c6721cd939401b2907e48d64ff6a880
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-15 00:48:48 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -30,7 +30,7 @@ class Triangle
       return false
     end
     #triangle inequality
-    if @sides[0] + @sides[1] < @sides[2] || @sides[0] + @sides[2] < @sides[1] || @sides[1] + @sides[2] < @sides[0]
+    if @sides[0] + @sides[1] <= @sides[2] || @sides[0] + @sides[2] <= @sides[1] || @sides[1] + @sides[2] <= @sides[0]
       return false
     end
 
