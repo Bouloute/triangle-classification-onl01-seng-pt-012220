47bac0f354f37dbdd0be320ebd9abf2380c94f4a
Anna Lewis Dagnino <annaick1995@gmail.com>
2020-02-15 00:28:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,7 +7,7 @@ class Triangle
   end
 
   def kind
-
+binding.pry
     if valid?
       side_a, side_b, side_c = @sides[0], @sides[1], @sides[2]
       if    ((side_a == side_b) && (side_a == side_c))
